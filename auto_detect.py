af1680d7923f139d955c75326487417ec8b7df30
tanya-suri <65773685+tanya-suri@users.noreply.github.com>
2022-05-28 22:11:34 +0530

    Update auto_detect.py

--- auto_detect.py
+++ auto_detect.py
@@ -4,13 +4,13 @@ import numpy as np
 
 
 def return_mode(img,thresh=80):
-  m=np.mean(im)
-  if m >80:
+  m = np.mean(im)
+  if m > 80:
     return "Day"
   else:
     return "Night"
 
-img='/content/images (8).jpeg'
+img = "image_1.jpeg"
 im = Image.open(img) 
-mode=return_mode(img)
+mode = return_mode(img)
 print(mode)
